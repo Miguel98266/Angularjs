5e66c614e3f2d2b777a5f663ed7996bef58149a5
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2021-08-13 14:08:48 -0500

    v1

--- http/controller.js
+++ http/controller.js
@@ -1,4 +1,55 @@
-var app = angular.module("MyFirsApp", []);
-app.controller("FirstController", function ($scope) {
-  
-});
+angular
+  .module("MyFirsApp", [])
+  .controller("FirstController", function ($scope, $http) {
+    $scope.posts = [];
+    $http({
+      method: "GET",
+      url: "https://jsonplaceholder.typicode.com/posts",
+    }).then(
+      function (response) {
+        $scope.posts = response.data;
+        console.log(response);
+      },
+      function (error) {}
+    );
+    $scope.addPost = function () {
+      $http({
+        method: "Post",
+        url: "https://jsonplaceholder.typicode.com/posts",
+        data: {
+          title: $scope.title,
+          body: $scope.body,
+          userId: 1,
+        },
+      }).then(function (response, status, headers, config) {
+        $scope.posts.push(response.data);
+
+      }),
+        function (error, status, headers, config) {
+          console.log(error);
+        };
+    };
+    /* La .success sintaxis era correcta hasta Angular v1.4.3.
+  $http.get("https://jsonplaceholder.typicode.com/posts")
+    .success(function(data){
+      console.log(data)
+    })
+    .error(function(err){
+
+    });
+    $http({
+      method: "Post",
+      url: "https://jsonplaceholder.typicode.com/posts",
+      title:$scope.newPost.title,
+      body:$scope.newPost.body,
+      userId:1
+    }).then(function(response,status,headers,config){
+      $scope.posts.push($scope,newPost);
+      $scope.addPost={};
+    })
+    , (function(error, status, headers, config) {
+      console.log(error);
+    });
+    
+    */
+  });
