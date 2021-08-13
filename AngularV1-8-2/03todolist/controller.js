66171a893b3e62f23205ebe4121b520ad3a54705
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2021-08-13 15:08:03 -0500

    v1

--- AngularV1-8-2/03todolist/controller.js
+++ AngularV1-8-2/03todolist/controller.js
@@ -1,16 +1,27 @@
 angular
   .module("ToDoList", ["LocalStorageModule"])
   .controller("ToDoController", function ($scope, localStorageService) {
-    $scope.todo=[];
+    if (localStorageService.get("angular-todolist")) {
+      $scope.todo = localStorageService.get("angular-todolist");
+    } else {
+      $scope.todo = [];
+    }
     /*
     {
-      actividad:'Terminar curso',
+      descripcion:'Terminar curso',
       fecha:'13/08/2021
     }
     */
-   $scope.addActv=function(){
-    $scope.todo.push($scope.newActv);
-    $scope.newActv={};
-   }
-   
+
+    $scope.$watchCollection("todo", function (newValue, oldValue) {
+      localStorageService.set("angular-todolist", $scope.todo);
+    });
+    $scope.addActv = function () {
+      $scope.todo.push($scope.newActv);
+      $scope.newActv = {};
+    };
+
+    $scope.clean = function () {
+      $scope.todo = [];
+    };
   });
