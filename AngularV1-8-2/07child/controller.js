d5483e494276c7be68b3ce3d0b072a1c8061e1d0
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2021-08-15 02:04:27 -0500

    v1

--- AngularV1-8-2/07child/controller.js
+++ AngularV1-8-2/07child/controller.js
@@ -1,5 +1,14 @@
 angular
   .module("MyFirsApp", [])
+  .run(function ($rootScope) {
+    $rootScope.nombre = "Miguel";
+  })
   .controller("FirstController", function ($scope) {
-
-  });
+    $scope.nombre = "CF";
+    setTimeout(function () {
+      $scope.apply(function () {
+        $scope.nombre = "xD";
+      });
+    }, 1000);
+  })
+  .controller("ChildController", function ($scope) {});
