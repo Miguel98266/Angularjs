d832af0fbe101b37c710c25d3a4b46bf51976e2b
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2021-08-16 02:24:21 -0500

    v1

--- AngularV1-8-2/11ng-src/app.js
+++ AngularV1-8-2/11ng-src/app.js
@@ -1,16 +1,60 @@
-angular.module("CustomDirective",[])
-
-.controller("AppContrl",function($scope,$http){
-    $http({
-        method: "GET",
-        url: "https://api.github.com/users/urielhdz/repos",
-      }).then(
-        function (response) {
-          $scope.repos = response.data;
-          console.log(response);
+angular
+  .module("CustomDirective", [])
+  .directive("myAutocomplete", function () {
+    function link(scope, element, attrs) {
+      $(element).autocomplete({
+        source: scope[attrs.myAutocomplete],
+        select: function (ev, ui) {
+          ev.preventDefault();
+          if (ui.item) {
+              scope.optionSelected(ui.item.value)
+          }
         },
-        function (error) {
-            console.log(error)
+        focus: function (ev, ui) {
+            ev.preventDefault();
+            $(this).val(ui.item.label);
+        },
+      });
+    }
+    return {
+      link: link,
+    };
+  })
+  .directive("backImg", function () {
+    return function ($scope, element, attrs) {
+      attrs.$observe("backImg", function (value) {
+        element.css({
+          background: "url(" + value + ")",
+          "background-size": "cover",
+          "background-position": "center",
+        });
+      });
+    };
+  })
+  /*
+style="background:url({{repo.owner.avatar_url}});background-position:center; background-size:cover;
+*/
+  .controller("AppContrl", function ($scope, $http) {
+    $scope.repos = [];
+    $http({
+      method: "GET",
+      url: "https://api.github.com/users/urielhdz/repos",
+    }).then(
+      function (response) {
+        $scope.posts = response.data;
+        console.log(response.data.length);
+        for (var i = response.data.length - 1; i >= 0; i--) {
+          var repo = response.data[i];
+          $scope.repos.push(repo.name);
         }
-      );
-});
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+    $scope.optionSelected=function(data){
+        $scope.$apply(function(){
+            $scope.main_repo=data;
+        })
+    }
+  });
---
+++ AngularV1-8-2/org/app.js
@@ -0,0 +1,65 @@
+angular
+  .module("MyFirsApp", [])
+  .controller("mainCtrl", function AppCtrl($scope) {
+    $scope.data = [
+      {
+        name: "Ian Devling",
+        area: "Corporate",
+        office: "CTO office",
+        positionName: "Chief Executive Officer",
+        id: "O-6066",
+        parentId: "",
+      },
+      {
+        name: "Davolio Nancy",
+        area: "Corporate",
+        office: "CEO office",
+        positionName: "CTO",
+        id: "O-6067",
+        parentId: "O-6066",
+      },
+    ];
+    chart = new d3.OrgChart()
+      .container(".chart-container")
+      .data($scope.data)
+      .nodeWidth((d) => 250)
+      .nodeHeight((d) => 175)
+      .childrenMargin((d) => 40)
+      .compactMarginBetween((d) => 15)
+      .compactMarginPair((d) => 80)
+      .nodeContent(function (d, i, arr, state) {
+        return `
+        <div style="padding-top:30px;background-color:none;margin-left:1px;height:${
+          d.height
+        }px;border-radius:2px;overflow:visible">
+          <div style="height:${
+            d.height - 32
+          }px;padding-top:0px;background-color:white;border:1px solid lightgray;">
+
+
+           <div style="margin-right:10px;margin-top:15px;float:right">${
+             d.data.id
+           }</div>
+           
+           <div style="margin-top:-10px;background-color:#3AB6E3;height:10px;width:${
+             d.width - 2
+           }px;border-radius:1px"></div>
+
+           <div style="padding:20px; padding-top:30px;text-align:center">
+               <div style="color:#111672;font-size:16px;font-weight:bold"> ${
+                 d.data.name
+               } </div>
+               <div style="color:#404040;font-size:16px;margin-top:4px"> ${
+                 d.data.positionName
+               } </div>
+           </div> 
+           <div style="display:flex;padding-left:15px;padding-right:15px;">
+             <div > Gestiona:  ${d.data._directSubordinates} 👤</div>  
+             <div > Supervisa: ${d.data._totalSubordinates} 👤</div>    
+           </div>
+          </div>     
+  </div>
+`;
+      })
+      .render();
+  });
